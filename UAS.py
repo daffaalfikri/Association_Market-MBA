3bfbaa6d89174f0f375f27ab0cb0c829b2a5742c
daffaalfikri <149288996+daffaalfikri@users.noreply.github.com>
2023-12-24 19:58:22 +0700

    UAS.py

--- UAS.py
+++ UAS.py
@@ -21,15 +21,12 @@ def user_input_features():
     itemDescription = st.selectbox("itemDescription", ['tropical fruit', 'whole milk', 'pip fruit',
                                    'other vegetables', 'whole milk', 'rolls/buns', 'other vegetables', 'pot plants', 'whole milk'])
     Mounth = st.select_slider("Mounth", list(map(str, range(1, 12))))
-    print("itemDescription:", itemDescription)
-    print("year:", year)
-    print("Product:", Product)
     return itemDescription, year, Product
 
 itemDescription, year, Product = user_input_features()
 
 
-data = get_data(itemDescription.lower(), year)
+data = get_data(itemDescription.lower(), Mounth)
 
 
 def encode(x):
